41afa4d8d1a3503ad9a70bcce0c7fc60b568d7cd
leaf-lxh <meleaf@qq.com>
2023-03-16 13:50:21 +0800

    add redisson config

---
+++ src/main/java/com/inkneko/heimusic/config/RedissonConfig.java
@@ -0,0 +1,21 @@
+package com.inkneko.heimusic.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+    @Value("${heimusic.redis.url}")
+    private String redisUrl;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress(redisUrl);
+        return Redisson.create(config);
+    }
+}
