d9480b279723addb8db9cc44bc9851937f4a870c
leaf-lxh <meleaf@qq.com>
2023-12-15 20:09:47 +0800

    添加版本说明

--- src/main/java/com/inkneko/heimusic/HeiMusicApplication.java
+++ src/main/java/com/inkneko/heimusic/HeiMusicApplication.java
@@ -1,9 +1,12 @@
 package com.inkneko.heimusic;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(description = "HeiMusic! 服务端API说明", version = "0.2.1"))
 public class HeiMusicApplication {
 
     public static void main(String[] args) {
