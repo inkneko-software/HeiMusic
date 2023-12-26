ee956e4455b31c9d62f709dcfa07ffce015ab4da
leaf-lxh <meleaf@qq.com>
2023-12-26 17:27:05 +0800

    ...

--- src/main/java/com/inkneko/heimusic/controller/MusicController.java
+++ src/main/java/com/inkneko/heimusic/controller/MusicController.java
@@ -3,7 +3,6 @@ package com.inkneko.heimusic.controller;
 import com.inkneko.heimusic.config.MinIOConfig;
 import com.inkneko.heimusic.config.RabbitMQConfig;
 import com.inkneko.heimusic.exception.ServiceException;
-import com.inkneko.heimusic.model.dto.AddMusicFromCueDto;
 import com.inkneko.heimusic.model.dto.MusicDto;
 import com.inkneko.heimusic.model.entity.Music;
 import com.inkneko.heimusic.model.vo.Response;
@@ -12,19 +11,15 @@ import com.inkneko.heimusic.rabbitmq.model.SplitRequest;
 import com.inkneko.heimusic.service.MinIOService;
 import com.inkneko.heimusic.service.MusicService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Data;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.print.attribute.standard.Media;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/music")
@@ -73,12 +68,12 @@ public class MusicController {
     @Operation(
             summary = "通过cue信息添加音乐",
             description = "cue文件中声明了一个或多个FILE，每个FILE包含了一组TRACK（单曲的音乐信息）。" +
-            "通过上传file与该文件所拥有的单曲信息musicList，后端将自动将整个音轨切为对应的单曲，并添加到指定专辑")
+                    "通过上传file与该文件所拥有的单曲信息musicList，后端将自动将整个音轨切为对应的单曲，并添加到指定专辑")
     @PostMapping(value = "/addMusicFromCue", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Response<List<Music>> addMusicFromCue(@RequestPart List<MusicDto> musicDtoList,
                                                  @RequestPart MultipartFile file,
                                                  BindingResult result) {
-        if (result.hasErrors()){
+        if (result.hasErrors()) {
             System.out.println(result);
             throw new ServiceException(400, "请求参数有误");
         }
@@ -105,14 +100,5 @@ public class MusicController {
         splitRequest.setMusicList(musicInfoList);
         amqpTemplate.convertAndSend(RabbitMQConfig.topicExchangeName, String.format(RabbitMQConfig.Split.routingKey, 12450), splitRequest);
         return new Response<>(0, "ok", musicList);
-    }
-    @Data
-    public static class TestDto {
-        Integer test1;
-        Integer test2;
-    }
-    @PostMapping(value = "/testPost", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Response<?> testPost(@RequestPart TestDto albumId){
-        return new Response<>(0, "ok", albumId);
     }
 }
