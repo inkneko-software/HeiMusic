835dae9674105324976605ae29cfdf1d37960a4d
leaf-lxh <meleaf@qq.com>
2024-01-09 22:30:20 +0800

    添加音乐推荐功能

--- src/main/java/com/inkneko/heimusic/controller/AlbumController.java
+++ src/main/java/com/inkneko/heimusic/controller/AlbumController.java
@@ -8,6 +8,7 @@ import com.inkneko.heimusic.exception.ServiceException;
 import com.inkneko.heimusic.model.dto.UpdateAlbumInfoDto;
 import com.inkneko.heimusic.model.entity.Album;
 import com.inkneko.heimusic.model.entity.Music;
+import com.inkneko.heimusic.model.entity.MusicArtist;
 import com.inkneko.heimusic.model.vo.*;
 import com.inkneko.heimusic.service.AlbumService;
 import com.inkneko.heimusic.service.ArtistService;
@@ -15,13 +16,11 @@ import com.inkneko.heimusic.service.MinIOService;
 import com.inkneko.heimusic.service.MusicService;
 import com.inkneko.heimusic.util.auth.AuthUtils;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -94,7 +93,7 @@ public class AlbumController {
     public Response<List<MusicVo>> getAlbumMusicList(@RequestParam Integer albumId) {
         Album album = albumService.getById(albumId);
         Integer userId = AuthUtils.auth();
-        if (album == null){
+        if (album == null) {
             throw new ServiceException(404, "指定专辑不存在");
         }
         List<MusicVo> musicVos = albumService.getAlbumMusicList(albumId)
@@ -138,7 +137,7 @@ public class AlbumController {
     @UserAuth
     @GetMapping("/getAlbumList")
     @Operation(summary = "查询已上传专辑")
-    public Response<AlbumListVo> getAlbumList(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer size){
+    public Response<AlbumListVo> getAlbumList(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer size) {
         List<AlbumVo> result = albumService.page(new Page<Album>(page, size), new LambdaQueryWrapper<Album>().orderByDesc(Album::getAlbumId))
                 .getRecords()
                 .stream()
@@ -155,24 +154,24 @@ public class AlbumController {
 
     @PostMapping(value = "/updateAlbumInfo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @Operation(summary = "更改专辑基础信息")
-    public Response<?> updateAlbumInfo(@ModelAttribute UpdateAlbumInfoDto updateAlbumInfoDto){
+    public Response<?> updateAlbumInfo(@ModelAttribute UpdateAlbumInfoDto updateAlbumInfoDto) {
         Album album = albumService.getById(updateAlbumInfoDto.getAlbumId());
-        if (album == null){
+        if (album == null) {
             throw new ServiceException(404, "专辑不存在");
         }
 
-        if (updateAlbumInfoDto.getCover() != null){
+        if (updateAlbumInfoDto.getCover() != null) {
             minIOService.upload("heimusic", String.format("cover/%d_front", album.getAlbumId()), updateAlbumInfoDto.getCover());
             album.setFrontCoverUrl(String.format("%s/heimusic/cover/%d_front", minIOConfig.getEndpoint(), album.getAlbumId()));
         }
 
-        if (updateAlbumInfoDto.isDeleteCover()){
+        if (updateAlbumInfoDto.isDeleteCover()) {
             album.setFrontCoverUrl(null);
         }
 
         album.setTitle(updateAlbumInfoDto.getTitle());
         albumService.updateById(album);
-        if (updateAlbumInfoDto.getArtistList() != null){
+        if (updateAlbumInfoDto.getArtistList() != null) {
             albumService.updateAlbumArtist(album.getAlbumId(), updateAlbumInfoDto.getArtistList());
         }
         return new Response<>(0, "ok");
@@ -181,8 +180,114 @@ public class AlbumController {
     @PostMapping("/removeAlbum")
     @Operation(summary = "删除专辑")
     @UserAuth(requireRootPrivilege = true)
-    public Response<?> removeAlbum(@RequestParam Integer albumId){
+    public Response<?> removeAlbum(@RequestParam Integer albumId) {
         albumService.removeById(albumId);
         return new Response<>(0, "ok");
+    }
+
+    @Operation(summary = "随机播放一首音乐")
+    @PostMapping("/randomMusic")
+    @UserAuth
+    public Response<MusicVo> randomMusic() {
+        Integer userId = AuthUtils.auth();
+        if (userId == null) {
+            throw new ServiceException(403, "请先登录");
+        }
+        Music lastMusic = musicService.getOne(new LambdaQueryWrapper<Music>().orderByDesc(Music::getMusicId).last("LIMIT 1"));
+        //需要把ID字段的Integer换成Long。数据库设计失误，所有的ID应当使用BIGINT
+        Random random = new Random(userId + new Date().getTime());
+
+        Music selectedMusic = null;
+        Album album = null;
+        while (selectedMusic == null || selectedMusic.getBucket() == null) {
+            Integer musicId = (int) random.nextLong() % lastMusic.getMusicId();
+            selectedMusic = musicService.getById(musicId);
+            if (selectedMusic != null) {
+                album = albumService.getAlbumMusicByMusicId(selectedMusic.getMusicId());
+                if (album == null) {
+                    selectedMusic = null;
+                }
+            }
+
+        }
+
+        List<ArtistVo> artistVos = musicService
+                .getMusicArtists(selectedMusic.getMusicId())
+                .stream()
+                .map(MusicArtist::getArtistId)
+                .map(artistId -> artistService.getById(artistId))
+                .map(ArtistVo::new)
+                .collect(Collectors.toList());
+        List<MusicResourceVo> musicResourceVos = musicService
+                .getMusicResources(selectedMusic.getMusicId())
+                .stream()
+                .map(musicResource -> new MusicResourceVo(musicResource, minIOConfig.getEndpoint()))
+                .collect(Collectors.toList());
+        return new Response<>(
+                0,
+                "ok",
+                new MusicVo(selectedMusic, album, artistVos, musicResourceVos, minIOConfig.getEndpoint(), musicService.isFavorite(userId, selectedMusic.getMusicId())));
+
+
+    }
+
+    @Operation(summary = "用户每日推荐30首（随机30首，无个性推荐功能）", description = "每日6点更新")
+    @PostMapping("/daily30")
+    @UserAuth
+    public Response<List<MusicVo>> daily30() {
+        Integer userId = AuthUtils.auth();
+        if (userId == null) {
+            throw new ServiceException(403, "请先登录");
+        }
+        long count = musicService.count();
+        if (count < 100) {
+            throw new ServiceException(500, "音乐数量小于100，请添加更多音乐后尝试");
+        }
+
+        Music lastMusic = musicService.getOne(new LambdaQueryWrapper<Music>().orderByDesc(Music::getMusicId).last("LIMIT 1"));
+        //每日6点更新
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 6);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        Random random = new Random(userId + calendar.getTimeInMillis());
+
+        List<MusicVo> recommendMusicList = new ArrayList<>();
+        HashSet<Integer> recommendSet = new HashSet<>();
+        for (int i = 0; i < 30; i++) {
+            Music selectedMusic = null;
+            Album album = null;
+
+            while (selectedMusic == null || selectedMusic.getBucket() == null) {
+                Integer musicId = (int) random.nextLong() % lastMusic.getMusicId();
+                selectedMusic = musicService.getById(musicId);
+                if (selectedMusic != null && !recommendSet.contains(selectedMusic.getMusicId())) {
+                    album = albumService.getAlbumMusicByMusicId(selectedMusic.getMusicId());
+                    if (album != null) {
+                        recommendSet.add(selectedMusic.getMusicId());
+                        continue;
+                    }
+                }
+                selectedMusic = null;
+
+            }
+            List<ArtistVo> artistVos = musicService
+                    .getMusicArtists(selectedMusic.getMusicId())
+                    .stream()
+                    .map(MusicArtist::getArtistId)
+                    .map(artistId -> artistService.getById(artistId))
+                    .map(ArtistVo::new)
+                    .collect(Collectors.toList());
+            List<MusicResourceVo> musicResourceVos = musicService
+                    .getMusicResources(selectedMusic.getMusicId())
+                    .stream()
+                    .map(musicResource -> new MusicResourceVo(musicResource, minIOConfig.getEndpoint()))
+                    .collect(Collectors.toList());
+            recommendMusicList.add(new MusicVo(selectedMusic, album, artistVos, musicResourceVos, minIOConfig.getEndpoint(), musicService.isFavorite(userId, selectedMusic.getMusicId())));
+        }
+        return new Response<>(0, "ok", recommendMusicList);
     }
 }
