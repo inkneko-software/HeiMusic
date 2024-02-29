9c8e3a9fb0f1ba1cd1a292d0adf987d62fcb3671
leaf-lxh <meleaf@qq.com>
2024-02-29 15:25:39 +0800

    添加实体类

---
+++ src/main/java/com/inkneko/heimusic/model/entity/PlaylistMusic.java
@@ -0,0 +1,20 @@
+package com.inkneko.heimusic.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlaylistMusic {
+    private Integer playlistId;
+    private Integer musicId;
+    private Date createdAt;
+    private Date updatedAt;
+
+}
