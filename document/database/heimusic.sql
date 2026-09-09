44e4b3a4bf3f37bc8166229adb2482c52cb90655
leaf-lxh <meleaf@qq.com>
2026-09-09 23:54:39 +0800

    feat: 跟进文档设计

--- document/database/heimusic.sql
+++ document/database/heimusic.sql
@@ -2,14 +2,19 @@ docker exec -i heimusic-mysql -uroot -ppassword < heimusic.sql
 CREATE DATABASE IF NOT EXISTS heimusic;
 USE heimusic;
 CREATE USER 'user'@'%' identified by 'password';
+CREATE TABLE IF NOT EXISTS heimusic_application_info(
+    version VARCHAR(255) NOT NULL DEFAULT 'current',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+)Engine=InnoDB Default Charset=UTF8MB4;
 CREATE TABLE IF NOT EXISTS user_detail(
     user_id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(20),
-    email VARCHAR(30),
+    username VARCHAR(255),
+    email VARCHAR(255),
     avatar_url VARCHAR(255) NOT NULL DEFAULT "/images/default_avatar.jpg",
     birth DATETIME,
     gender CHAR(1),
-    sign VARCHAR(100),
+    sign VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     UNIQUE(email)
@@ -29,9 +34,9 @@ CREATE TABLE IF NOT EXISTS user_auth(
 )Engine=InnoDB default Charset=UTF8MB4;
 CREATE TABLE IF NOT EXISTS artist(
     artist_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    translate_name VARCHAR(50),
-    avatar_url VARCHAR(100),
+    name VARCHAR(255) NOT NULL,
+    translate_name VARCHAR(255),
+    avatar_url VARCHAR(255) NOT NULL DEFAULT '',
     birth DATE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -41,8 +46,11 @@ CREATE TABLE IF NOT EXISTS album(
     album_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     translate_title VARCHAR(255),
-    front_cover_file VARCHAR(255),
-    back_cover_file VARCHAR(255),
+    front_cover_bucket VARCHAR(255) NOT NULL DEFAULT '' COMMENT '封面所在的桶',
+    front_cover_object_key VARCHAR(255) NOT NULL DEFAULT '' COMMENT '封面的对象标识',
+    front_cover_file_path VARCHAR(255) NOT NULL DEFAULT '' COMMENT '封面文件路径',
+    large_track_nums INT NOT NULL DEFAULT 0 COMMENT '专辑的整个抓取的音乐轨道数量(flac+cue)',
+    album_artist VARCHAR(255) NOT NULL DEFAULT 'V.A.' COMMENT '专辑艺术家字符串',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
 )Engine=InnoDB default charset=utf8mb4;
@@ -63,11 +71,21 @@ CREATE TABLE IF NOT EXISTS music(
     bucket VARCHAR(255) COMMENT '音乐文件的桶名称',
     object_key VARCHAR(255) COMMENT '音乐文件的对象名称，如"/路径/文件名.后缀"',
     bitrate VARCHAR(255) COMMENT '比特率，单位bs，如1904219',
-    codec VARCHAR(20) COMMENT '音乐编码格式，如FLAC',
-    duration VARCHAR(255) COMMENT '时长，以秒为单位, 如275.453333',
-    size VARCHAR(255) COMMENT '文件大小，以b为单位，如65565467' ,
+    codec VARCHAR(255) COMMENT '音乐编码格式，如FLAC',
+    duration VARCHAR(255) NOT NULL DEFAULT '0' COMMENT '时长，以秒为单位, 如275.453333',
+    size VARCHAR(255) COMMENT '文件大小，以b为单位，如65565467',
+    track_number INT NOT NULL DEFAULT 0 COMMENT '歌曲的编号',
+    track_total INT NOT NULL DEFAULT 0 COMMENT '碟片的歌曲总数',
+    disc_number INT NOT NULL DEFAULT 0 COMMENT '所在的碟片编号',
+    disc_total INT NOT NULL DEFAULT 0 COMMENT '当前专辑的碟片总数',
+    artist VARCHAR(255) NOT NULL DEFAULT '' COMMENT '该歌曲的艺术家（所有艺术家的名称）',
+    file_path VARCHAR(1024) NOT NULL DEFAULT '' COMMENT '该文件的路径',
+    file_hash VARCHAR(255) NOT NULL DEFAULT '' COMMENT '该文件的哈希值',
+    disc_start_time VARCHAR(255) NOT NULL DEFAULT '' COMMENT '音轨中音乐的起始时间，以秒为单位',
+    disc_end_time VARCHAR(255) NOT NULL DEFAULT '' COMMENT '音轨中音乐的结束时间，以秒为单位',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL
 )Engine=InnoDB default charset=utf8mb4;
 CREATE TABLE IF NOT EXISTS music_artist(
     music_id INT,
@@ -77,7 +95,7 @@ CREATE TABLE IF NOT EXISTS music_artist(
 CREATE TABLE IF NOT EXISTS music_resource(
     music_resource_id INT PRIMARY KEY AUTO_INCREMENT,
     music_id INT NOT NULL,
-    codec VARCHAR(20),
+    codec VARCHAR(255),
     bitrate VARCHAR(255),
     bucket VARCHAR(255),
     object_key VARCHAR(255),
@@ -95,14 +113,18 @@ CREATE TABLE IF NOT EXISTS music_favorite(
 CREATE TABLE IF NOT EXISTS playlist(
     playlist_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT '歌单id',
     user_id INT NOT NULL COMMENT '创建者用户id',
-    description VARCHAR(255) COMMENT '歌单简介',
+    title VARCHAR(255) NOT NULL DEFAULT '' COMMENT '歌单名称',
+    description VARCHAR(255) NOT NULL DEFAULT '' COMMENT '歌单简介',
+    sequence_number INT NOT NULL DEFAULT 0 COMMENT '自定义排序编号',
     cover_url VARCHAR(255) COMMENT '歌单封面',
+    play_count VARCHAR(255) NOT NULL DEFAULT 0 COMMENT '播放次数',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )Engine=InnoDB default charset=utf8mb4;
 CREATE TABLE IF NOT EXISTS playlist_music(
     playlist_id INT NOT NULL COMMENT '歌单id',
     music_id INT NOT NULL COMMENT '音乐id',
+    sequence_number INT NOT NULL DEFAULT 0 COMMENT '自定义排序编号',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY(playlist_id, music_id)
@@ -110,8 +132,9 @@ CREATE TABLE IF NOT EXISTS playlist_music(
 CREATE TABLE IF NOT EXISTS playlist_subscribe(
     playlist_id INT NOT NULL COMMENT '歌单id',
     user_id INT NOT NULL COMMENT '收藏者用户id',
+    sequence_number INT NOT NULL DEFAULT 0 COMMENT '自定义排序编号',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY(playlist_id, music_id)
+    PRIMARY KEY(playlist_id, user_id)
 )Engine=InnoDB default charset=utf8mb4;
 GRANT SELECT,DELETE,UPDATE,INSERT ON heimusic.* TO 'user'@'%';
